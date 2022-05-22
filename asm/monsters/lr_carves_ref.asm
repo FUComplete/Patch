2fe044ad9dab399a5fa5e0352ddb3f125a14cce6
IncognitoMan <IncognitoMan@users.noreply.github.com>
2022-05-21 20:56:07 -0400

    Added unranked and low rank carve tables.

---
+++ asm/monsters/lr_carves_ref.asm
@@ -0,0 +1,153 @@
+; Low rank carves ref
+;
+; Body carves (village/guild lr)
+;
+.definelabel RathianBodyLR,0x09CCF490
+.definelabel KelbiBodyLR,0x09CCF4B0
+.definelabel MosswineBodyLR,0x09CCF4C8
+.definelabel BullfangoBodyLR,0x09CCF4E0
+.definelabel KutKuBodyLR,0x09CCF4F8
+.definelabel CephadromeBodyLR,0x09CCF510
+.definelabel RathalosBodyLR,0x09CCF528
+.definelabel AptonothBodyLR,0x09CCF548
+.definelabel GenpreyBodyLR,0x09CCF558
+.definelabel DiablosBodyLR,0x09CCF568
+.definelabel KhezuBodyLR,0x09CCF578
+.definelabel VelocipreyBodyLR,0x09CCF588
+.definelabel GraviosBodyLR,0x09CCF5A0
+.definelabel VespoidBodyLR,0x09CCF5B8
+.definelabel GypcerosBodyLR,0x09CCF5C8
+.definelabel PlesiothBodyLR,0x09CCF5E0
+.definelabel BasariosBodyLR,0x09CCF5F0
+.definelabel UnkBody1LR,0x09CCF608 ; 100% nothing
+.definelabel HornetaurBodyLR,0x09CCF610
+.definelabel ApcerosBodyLR,0x09CCF628
+.definelabel MonoblosBodyLR,0x09CCF638
+.definelabel VelocidromeBodyLR,0x09CCF650
+.definelabel GendromeBodyLR,0x09CCF668
+.definelabel IopreyBodyLR,0x09CCF680
+.definelabel IodromeBodyLR,0x09CCF690
+.definelabel KirinBodyLR,0x09CCF6A8
+.definelabel CephalosBodyLR,0x09CCF6C0
+.definelabel GiapreyBodyLR,0x09CCF6D8
+.definelabel PinkRathianBodyLR,0x09CCF6E8
+.definelabel BlueKutKuBodyLR,0x09CCF708
+.definelabel PurpGypcerosBodyLR,0x09CCF720
+.definelabel OneEarGarugaBodyLR,0x09CCF738
+.definelabel UnkBody2LR,0x09CCF750 ; (Silver Rathalos) 20% FireWyvern Fluid, 24% Rathalos Webbing, 27% Flame Sac, 25% Monster Bone+, 4% FireWyvrn Marrow
+.definelabel UnkBody3LR,0x09CCF768 ; (Gold Rathian) 25% FireWyvern Fluid, 5% Rathian Spike, 30% Flame Sac, 37% Monster Bone+, 3% FireWyvrn Marrow
+.definelabel BlkDiablosBodyLR,0x09CCF780
+.definelabel WhiteMonoblosBodyLR,0x09CCF790
+.definelabel RedKhezuBodyLR,0x09CCF7A8
+.definelabel GrnPlesiothBodyLR,0x09CCF7B8
+.definelabel BlkGraviosBodyLR,0x09CCF7C8
+.definelabel DHermitaurBodyLR,0x09CCF7E0
+.definelabel AzureRathalosBodyLR,0x09CCF7F8
+.definelabel BlangongaBodyLR,0x09CCF818
+.definelabel CongalalaBodyLR,0x09CCF830
+.definelabel RajangBodyLR,0x09CCF840
+.definelabel KushalaBodyLR,0x09CCF858
+.definelabel ShakalakaBodyLR,0x09CCF870
+.definelabel ChameleosBodyLR,0x09CCF890
+.definelabel UnkBody4LR,0x09CCF8A8 ; (Rusted Daora) 32% Daora Shell, 50% DaoraDragonScale, 5% Daora Claw, 10% Eldr Dragon Bone, 3% ElderDragonBlood
+.definelabel BlangoBodyLR,0x09CCF8C0
+.definelabel CongaBodyLR,0x09CCF8D0
+.definelabel RemobraBodyLR,0x09CCF8E0
+.definelabel LunastraBodyLR,0x09CCF8F0
+.definelabel TeostraBodyLR,0x09CCF910
+.definelabel HermitaurBodyLR,0x09CCF930
+.definelabel ShogunCeanataurBodyLR,0x09CCF940
+.definelabel BulldromeBodyLR,0x09CCF958
+.definelabel AntekaBodyLR,0x09CCF970
+.definelabel PopoBodyLR,0x09CCF988
+.definelabel CeanataurBodyLR,0x09CCF998
+.definelabel TigrexBodyLR,0x09CCF9B0
+.definelabel AkantorBodyLR,0x09CCF9C8
+.definelabel GiadromeBodyLR,0x09CCF9D8
+.definelabel GarugaBodyLR,0x09CCF9F0
+.definelabel KingShakalakaBodyLR,0x09CCFA08
+.definelabel QueenVespoidBodyLR,0x09CCFA28
+.definelabel NargacugaBodyLR,0x09CCFA38
+.definelabel HypnocatriceBodyLR,0x09CCFA48
+.definelabel UnkBody5LR,0x09CCFA58 ; (Lavasioth) 100% Dung
+.definelabel UnkBody6LR,0x09CCFA60 ; (Copper Blangonga) 100% Dung
+.definelabel UnkBody7LR,0x09CCFA68 ; (Emerald Congalala) 100% Dung
+.definelabel UnkBody8LR,0x09CCFA70 ; (Plum D Hermitaur) 100% Dung
+.definelabel UnkBody9LR,0x09CCFA78 ; (Terra Ceanataur) 100% Dung
+.definelabel UnkBody10LR,0x09CCFA80 ; (Ukanlos) 100% Dung
+.definelabel FuriousRajangBodyLR,0x09CCFA88
+;
+; Tail carves (village/guild lr)
+;
+.definelabel RathianTailLR,0x09CCFAA0
+.definelabel RathalosTailLR,0x09CCFAC0
+.definelabel DiablosTailLR,0x09CCFAD8
+.definelabel GraviosTailLR,0x09CCFAE8
+.definelabel BasariosTailLR,0x09CCFAF8
+.definelabel MonoblosTailLR,0x09CCFB08
+.definelabel PinkRathianTailLR,0x09CCFB18
+.definelabel OneEarGarugaTailLR,0x09CCFB38
+.definelabel UnkTail1LR,0x09CCFB48 ; 100% nothing
+.definelabel UnkTail2LR,0x09CCFB50 ; 100% nothing
+.definelabel BlkDiablosTailLR,0x09CCFB58
+.definelabel WhiteMonoblosTailLR,0x09CCFB68
+.definelabel BlkGraviosTailLR,0x09CCFB78
+.definelabel AzureRathalosTailLR,0x09CCFB88
+.definelabel KushalaTailLR,0x09CCFBA0
+.definelabel ChameleosTailLR,0x09CCFBB0
+.definelabel RustKhusalaTailLR,0x09CCFBC0
+.definelabel LunastraTailLR,0x09CCFBD0
+.definelabel TeostraTailLR,0x09CCFBE0
+.definelabel TigrexTailLR,0x09CCFBF0
+.definelabel UnkTail3LR,0x09CCFC00 ; 100% nothing
+.definelabel GarugaTailLR,0x09CCFC08
+.definelabel UnkTail4LR,0x09CCFC18 ; 100% Dung
+.definelabel UnkTail5LR,0x09CCFC20 ; 100% Dung
+;
+; Shiny drops (village/guild lr)
+;
+.definelabel RathianShinyLR,0x09CCFC28
+.definelabel KutKuShinyLR,0x09CCFC38
+.definelabel CephadromeShinyLR,0x09CCFC48
+.definelabel RathalosShinyLR,0x09CCFC58
+.definelabel DiablosShinyLR,0x09CCFC68
+.definelabel KhezuShinyLR,0x09CCFC78
+.definelabel GraviosShinyLR,0x09CCFC88
+.definelabel GypcerosShinyLR,0x09CCFC98
+.definelabel PlesiothShinyLR,0x09CCFCA8
+.definelabel BasariosShinyLR,0x09CCFCB8
+.definelabel MelynxShinyLR,0x09CCFCC8
+.definelabel MonoblosShinyLR,0x09CCFCE0
+.definelabel PinkRathianShinyLR,0x09CCFCF0
+.definelabel BlueKutKuShinyLR,0x09CCFD00
+.definelabel PurpGypcerosShinyLR,0x09CCFD10
+.definelabel OneEarGarugaShinyLR,0x09CCFD20
+.definelabel UnkShiny1LR,0x09CCFD30 ; 100% nothing
+.definelabel UnkShiny2LR,0x09CCFD38 ; 100% nothing
+.definelabel BlkDiablosShinyLR,0x09CCFD40
+.definelabel WhiteMonoblosShinyLR,0x09CCFD50
+.definelabel RedKhezuShinyLR,0x09CCFD60
+.definelabel GrnPlesiothShinyLR,0x09CCFD70
+.definelabel BlkGraviosShinyLR,0x09CCFD80
+.definelabel DHermitaurShinyLR,0x09CCFD90
+.definelabel AzureRathalosShinyLR,0x09CCFDA0
+.definelabel BlangongaShinyLR,0x09CCFDB0
+.definelabel KushalaShinyLR,0x09CCFDC0
+.definelabel GrtThunderbugShinyLR,0x09CCFDD8
+.definelabel ShakalakaShinyLR,0x09CCFDE8
+.definelabel ChameleosShinyLR,0x09CCFE18
+.definelabel RustKushalaShinyLR,0x09CCFE28
+.definelabel LunastraShinyLR,0x09CCFE40
+.definelabel TeostraShinyLR,0x09CCFE50
+.definelabel ShogunCeanataurShinyLR,0x09CCFE60
+.definelabel TigrexShinyLR,0x09CCFE70
+.definelabel UnkShiny3LR,0x09CCFE80 ; 100% nothing
+.definelabel GarugaShinyLR,0x09CCFE88
+.definelabel KingShakalakaShinyLR,0x09CCFE98
+.definelabel UnkShiny4LR,0x09CCFEC8 ; 100% Dung
+.definelabel UnkShiny5LR,0x09CCFED0 ; 100% Dung
+.definelabel UnkShiny6LR,0x09CCFED8 ; 100% Dung
+.definelabel UnkShiny7LR,0x09CCFEE0 ; 100% Dung
+.definelabel UnkShiny8LR,0x09CCFEE8 ; 100% Dung
+.definelabel UnkShiny9LR,0x09CCFEF0 ; 100% Dung
+.definelabel UnkShiny10LR,0x09CCFEF8 ; 100% Dung
---
+++ asm/monsters/ur_carves_ref.asm
@@ -0,0 +1,153 @@
+; Unrank carves ref
+;
+; Body carves (guild 1/2 star)
+;
+.definelabel RathianBodyUR,0x09CCECE0 
+.definelabel KelbiBodyUR,0x09CCECF8 
+.definelabel MosswineBodyUR,0x09CCED10 
+.definelabel BullfangoBodyUR,0x09CCED20 
+.definelabel KutKuBodyUR,0x09CCED30 
+.definelabel CephadromeBodyUR,0x09CCED48 
+.definelabel RathalosBodyUR,0x09CCED58  
+.definelabel AptonothBodyUR,0x09CCED70 
+.definelabel GenpreyBodyUR,0x09CCED80  
+.definelabel DiablosBodyUR,0x09CCED98 
+.definelabel KhezuBodyUR,0x09CCEDA8 
+.definelabel VelocipreyBodyUR,0x09CCEDC0 
+.definelabel GraviosBodyUR,0x09CCEDD8  
+.definelabel VespoidBodyUR,0x09CCEDF0  
+.definelabel GypcerosBodyUR,0x09CCEE00  
+.definelabel PlesiothBodyUR,0x09CCEE18  
+.definelabel BasariosBodyUR,0x09CCEE30  
+.definelabel UnkBody1UR,0x09CCEE48 ; 100% nothing
+.definelabel HornetaurBodyUR,0x09CCEE50  
+.definelabel ApcerosBodyUR,0x09CCEE60  
+.definelabel MonoblosBodyUR,0x09CCEE70  
+.definelabel VelocidromeBodyUR,0x09CCEE88  
+.definelabel GendromeBodyUR,0x09CCEE98  
+.definelabel IopreyBodyUR,0x09CCEEA8  
+.definelabel IodromeBodyUR,0x09CCEEC0  
+.definelabel KirinBodyUR,0x09CCEED0  
+.definelabel CephalosBodyUR,0x09CCEEE0  
+.definelabel GiapreyBodyUR,0x09CCEEF0  
+.definelabel PinkRathianBodyUR,0x09CCEF00  
+.definelabel BlueKutKuBodyUR,0x09CCEF18  
+.definelabel PurpGypcerosBodyUR,0x09CCEF28  
+.definelabel OneEarGarugaBodyUR,0x09CCEF40  
+.definelabel UnkBody2UR,0x09CCEF50 ; (Silver Rathalos) 100% nothing
+.definelabel UnkBody3UR,0x09CCEF58 ; (Gold Rathian) 100% nothing
+.definelabel BlkDiablosBodyUR,0x09CCEF60  
+.definelabel WhiteMonoblosBodyUR,0x09CCEF70  
+.definelabel RedKhezuBodyUR,0x09CCEF88  
+.definelabel GrnPlesiothBodyUR,0x09CCEFA0  
+.definelabel BlkGraviosBodyUR,0x09CCEFB8  
+.definelabel DHermitaurBodyUR,0x09CCEFD0  
+.definelabel AzureRathalosBodyUR,0x09CCEFE8  
+.definelabel BlangongaBodyUR,0x09CCF000  
+.definelabel CongalalaBodyUR,0x09CCF010  
+.definelabel RajangBodyUR,0x09CCF020 
+.definelabel KushalaBodyUR,0x09CCF030 
+.definelabel ShakalakaBodyUR,0x09CCF040 
+.definelabel ChameleosBodyUR,0x09CCF050 
+.definelabel UnkBody4UR,0x09CCF068 ; (Rusted Daora) 35% Daora Shell, 50% DaoraDragonScale, 15% Eldr Dragon Bone
+.definelabel BlangoBodyUR,0x09CCF078 
+.definelabel CongaBodyUR,0x09CCF088 
+.definelabel RemobraBodyUR,0x09CCF098  
+.definelabel LunastraBodyUR,0x09CCF0A8 
+.definelabel TeostraBodyUR,0x09CCF0C0 
+.definelabel HermitaurBodyUR,0x09CCF0D8 
+.definelabel ShogunCeanataurBodyUR,0x09CCF0E8 
+.definelabel BulldromeBodyUR,0x09CCF100 
+.definelabel AntekaBodyUR,0x09CCF110 
+.definelabel PopoBodyUR,0x09CCF128  
+.definelabel CeanataurBodyUR,0x09CCF138 
+.definelabel TigrexBodyUR,0x09CCF148 
+.definelabel AkantorBodyUR,0x09CCF158 ; 100% nothing
+.definelabel GiadromeBodyUR,0x09CCF160 
+.definelabel GarugaBodyUR,0x09CCF170 
+.definelabel KingShakalakaBodyUR,0x09CCF180 
+.definelabel QueenVespoidBodyUR,0x09CCF190 ; 100% Dung
+.definelabel NargacugaBodyUR,0x09CCF198 ; 100% Med Monster Bone
+.definelabel HypnocatriceBodyUR,0x09CCF1A0 ; 100% Dung
+.definelabel UnkBody5UR,0x09CCF1A8 ; (Lavasioth) 100% Dung
+.definelabel UnkBody6UR,0x09CCF1B0 ; (Copper Blangonga) 100% Dung
+.definelabel UnkBody7UR,0x09CCF1B8 ; (Emerald Congalala) 100% Demondrug
+.definelabel UnkBody8UR,0x09CCF1C0 ; (Plum D Hermitaur) 100% Dung
+.definelabel UnkBody9UR,0x09CCF1C8 ; (Terra Ceanataur) 100% Akito Jewel
+.definelabel UnkBody10UR,0x09CCF1D0 ; (Ukanlos) 100% Dung
+.definelabel FuriousRajangBodyUR,0x09CCF1D8
+;
+; Tail carves (guild 1/2 star)
+;
+.definelabel RathianTailUR,0x09CCF1E8  
+.definelabel RathalosTailUR,0x09CCF1F8  
+.definelabel DiablosTailUR,0x09CCF208 ; 100% nothing
+.definelabel GraviosTailUR,0x09CCF210 ; 100% nothing
+.definelabel BasariosTailUR,0x09CCF218 ; 100% nothing
+.definelabel MonoblosTailUR,0x09CCF220 ; 100% nothing
+.definelabel PinkRathianTailUR,0x09CCF228 ; 100% nothing
+.definelabel OneEarGarugaTailUR,0x09CCF230 ; 100% nothing
+.definelabel UnkTail1UR,0x09CCF238 ; 100% nothing
+.definelabel UnkTail2UR,0x09CCF240 ; 100% nothing
+.definelabel BlkDiablosTailUR,0x09CCF248 ; 100% nothing
+.definelabel WhiteMonoblosTailUR,0x09CCF250 ; 100% nothing
+.definelabel BlkGraviosTailUR,0x09CCF258 ; 100% nothing
+.definelabel AzureRathalosTailUR,0x09CCF260 ; 100% nothing
+.definelabel KushalaTailUR,0x09CCF268 ; 100% nothing
+.definelabel ChameleosTailUR,0x09CCF270 ; 100% nothing
+.definelabel RustKhusalaTailUR,0x09CCF278 ; 100% nothing
+.definelabel LunastraTailUR,0x09CCF280 ; 100% nothing
+.definelabel TeostraTailUR,0x09CCF288 ; 100% nothing
+.definelabel TigrexTailUR,0x09CCF290 ; 100% Tigrex Scale
+.definelabel UnkTail3UR,0x09CCF298 ; 100% nothing
+.definelabel GarugaTailUR,0x09CCF2A0 ; 100% Dung
+.definelabel UnkTail4UR,0x09CCF2A8 ; 100% Mega Potion
+.definelabel UnkTail5UR,0x09CCF2B0 ; 100% Dung
+;
+; Shiny drops (guild 1/2 star)
+;
+.definelabel RathianShinyUR,0x09CCF2B8 - 
+.definelabel KutKuShinyUR,0x09CCF2C8 - 
+.definelabel CephadromeShinyUR,0x09CCF2D8 - 
+.definelabel RathalosShinyUR,0x09CCF2E8 -
+.definelabel DiablosShinyUR,0x09CCF2F8 ; 100% nothing
+.definelabel KhezuShinyUR,0x09CCF300 - 
+.definelabel GraviosShinyUR,0x09CCF310 ; 100% nothing
+.definelabel GypcerosShinyUR,0x09CCF318 - 
+.definelabel PlesiothShinyUR,0x09CCF328 - 
+.definelabel BasariosShinyUR,0x09CCF338 ; 100% nothing
+.definelabel MelynxShinyUR,0x09CCF340 -
+.definelabel MonoblosShinyUR,0x09CCF350 ; 100% nothing
+.definelabel PinkRathianShinyUR,0x09CCF358 ; 100% nothing
+.definelabel BlueKutKuShinyUR,0x09CCF360 ; 100% nothing
+.definelabel PurpGypcerosShinyUR,0x09CCF368 ; 100% nothing
+.definelabel OneEarGarugaShinyUR,0x09CCF370 ; 100% nothing
+.definelabel UnkShiny1UR,0x09CCF378 ; 100% nothing
+.definelabel UnkShiny2UR,0x09CCF380 ; 100% nothing
+.definelabel BlkDiablosShinyUR,0x09CCF388 ; 100% nothing
+.definelabel WhiteMonoblosShinyUR,0x09CCF390 ; 100% nothing
+.definelabel RedKhezuShinyUR,0x09CCF398 ; 100% nothing
+.definelabel GrnPlesiothShinyUR,0x09CCF3A0 ; 100% nothing
+.definelabel BlkGraviosShinyUR,0x09CCF3A8 ; 100% nothing
+.definelabel DHermitaurShinyUR,0x09CCF3B0 -
+.definelabel AzureRathalosShinyUR,0x09CCF3C0 ; 100% nothing
+.definelabel BlangongaShinyUR,0x09CCF3C8 - 
+.definelabel KushalaShinyUR,0x09CCF3D8 ; 100% nothing
+.definelabel GrtThunderbugShinyUR,0x09CCF3E0 - 
+.definelabel ShakalakaShinyUR,0x09CCF3F0 - 
+.definelabel ChameleosShinyUR,0x09CCF400 ; 100% nothing
+.definelabel RustKushalaShinyUR,0x09CCF408 ; 100% nothing
+.definelabel LunastraShinyUR,0x09CCF410 ; 100% nothing
+.definelabel TeostraShinyUR,0x09CCF418 ; 100% nothing
+.definelabel ShogunCeanataurShinyUR,0x09CCF420 - 
+.definelabel TigrexShinyUR,0x09CCF430 - 
+.definelabel UnkShiny3UR,0x09CCF440 ; 100% nothing
+.definelabel GarugaShinyUR,0x09CCF448 ; 100% Dung
+.definelabel KingShakalakaShinyUR,0x09CCF450 ; 100% Dung
+.definelabel UnkShiny4UR,0x09CCF458 ; 100% Mega Potion
+.definelabel UnkShiny5UR,0x09CCF460 ; 100% Dung
+.definelabel UnkShiny6UR,0x09CCF468 ; 100% Dung
+.definelabel UnkShiny7UR,0x09CCF470 ; 100% Dung
+.definelabel UnkShiny8UR,0x09CCF478 ; 100% Dung
+.definelabel UnkShiny9UR,0x09CCF480 ; 100% Mega Potion
+.definelabel UnkShiny10UR,0x09CCF488 ; 100% Dung
